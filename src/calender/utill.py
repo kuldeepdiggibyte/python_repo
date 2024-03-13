dea66cd1cbeedca721133df3c810c5580c0fba1a
kuldeepdiggibyte <kp.managuli2000@gmail.com>
2024-03-14 01:53:12 +0530

    calender program takes date as input and returns correspondingday

--- src/calender/utill.py
+++ src/calender/utill.py
@@ -1,7 +1,11 @@
 import calendar
+import logging
+
+import logging
+logging.basicConfig(level=logging.INFO , format='%(message)s')
 def calc():
     m, d, y = map(int, input().split())
     n = calendar.weekday(y, m, d)
     day = calendar.day_name[n]
-    print(day.upper())
+    logging.info(day.upper()) #logging.info("{:.02f}".format(average_score))
     return day.upper()
