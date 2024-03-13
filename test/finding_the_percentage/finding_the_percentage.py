47472cf98337460008584d5a8ca32778bfb415fe
kuldeepdiggibyte <kp.managuli2000@gmail.com>
2024-03-13 15:28:45 +0530

    modified test-cases

--- test/finding_the_percentage/finding_the_percentage.py
+++ test/finding_the_percentage/finding_the_percentage.py
@@ -1,8 +1,35 @@
 import unittest
 
+from src.finding_the_percentage.utill import student_avg
+
+
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_1(self):
+        self.assertEqual(student_avg(), 60.00)  # add assertion here
+
+
+    ''' 2
+        kuldeep 50 60 70 
+        amit 34 24 65
+        kuldeep'''
+
+    def test_2(self):
+        self.assertEqual(student_avg(), 30.00)
+
+    ''' 2
+        kuldeep 20 30 40 
+        amit 23 38 80
+        kuldeep'''
+
+    def test_3(self):
+        self.assertEqual(student_avg(), 70.00)
+
+    '''3
+    kuldeep 40 23 54
+    amit    34 23 43
+    krishna 60 70 80
+    krishna'''
+
 
 if __name__ == '__main__':
     unittest.main()
--- test/mutation/mutation.py
+++ test/mutation/mutation.py
@@ -1,8 +1,28 @@
 import unittest
 
+from src.mutation.utill import mutate_string
+
+
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_1(self):
+        self.assertEqual(mutate_string("asdes", 2, 'k'),"askes" )# add assertion here
+
+    '''string = asdes, position = 2, character(to replace) = k
+    expected output = askdes'''
+
+
+    def test_2(self):
+        self.assertEqual(mutate_string("amit", 0, 'q'),"qmit" )
+
+    '''string = amit, position = 0, character(to replace) = q
+        expected output = qmit'''
+
+    def test_3(self):
+        self.assertEqual(mutate_string("valrngalre", 3, 'k'),"valkngalre" )
+
+    '''string = valrngalre, position = 3, character(to replace) = k
+        expected output = valkngalre '''
+
 
 if __name__ == '__main__':
     unittest.main()
