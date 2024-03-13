9d5d8fc08d01edf8fe7b9923e2c3d076af7b4a51
kuldeepdiggibyte <kp.managuli2000@gmail.com>
2024-03-14 01:56:00 +0530

    modified test-cases

--- test/calender/calender.py
+++ test/calender/calender.py
@@ -1,8 +1,27 @@
 import unittest
 
+from src.calender.utill import calc
+
+
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_1(self):
+        self.assertEqual(calc(), "WEDNESDAY")# add assertion here
+
+    '''08 05 2015
+    WEDNESDAY'''
+
+    def test_2(self):
+        self.assertEqual(calc(), "SUNDAY")
+
+    '''12 03 2000
+    SUNDAY'''
+
+    def test_3(self):
+        self.assertEqual(calc(), "FRIDAY")
+
+    '''08 05 2016
+FRIDAY'''
+
 
 if __name__ == '__main__':
     unittest.main()
